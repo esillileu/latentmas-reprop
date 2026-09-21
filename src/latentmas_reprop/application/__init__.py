8b39fc350de1985f8c5aeb65a38a411f274a3fac
esillileu <esillileu@gmail.com>
2026-09-21 19:00:36 +0900

    feat(application): implement InterventionUseCase for own/cross/zero latent evaluation

--- src/latentmas_reprop/application/__init__.py
+++ src/latentmas_reprop/application/__init__.py
@@ -1,4 +1,5 @@
 from .benchmark_use_case import BenchmarkUseCase
 from .evaluation_service import evaluate_predictions
+from .intervention_use_case import InterventionUseCase
 
-__all__ = ["BenchmarkUseCase", "evaluate_predictions"]
+__all__ = ["BenchmarkUseCase", "InterventionUseCase", "evaluate_predictions"]
